27c26a5adbb6f540e6a11be51d215e89e9bad563
Your Name <you@example.com>
2024-12-13 09:49:37 +0100

    fix select page alignement

---
+++ weather-app/src/components/LanguageSelect.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const languages = [
+    {
+        id: 0,
+        flag: './fr.png',
+        value: 'fr',
+    },
+    {
+        id: 1,
+        flag: 'en.png',
+        value: 'en',
+    },
+    {
+        id: 2,
+        flag: 'de.png',
+        value: 'de',
+    }
+]
+</script>
+
+<template>
+    <v-menu>
+        <template v-slot:activator="{ props }">
+            <v-btn icon="mdi-translate" variant="text" v-bind="props" size="x-large" />
+        </template>
+        <v-list>
+            <v-list-item v-for="lang in languages" :key="lang.id" :value="lang.value">
+                <v-list-item-title>
+                    <v-img :src="lang.flag" width="25" />
+                </v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-menu>
+</template>
